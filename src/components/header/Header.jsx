35958f7cf0d8e947803b80c4ccc10e8ace03ef0b
GenaEphirov <107998074+GenaEphirov@users.noreply.github.com>
2022-09-18 14:04:47 +0300

    working on Header

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -8,13 +8,17 @@ const Header = props => {
   return (
     <header>
       <div className={style.container}>
-        <div className={style.logo}>
-          <a href="#"><img src="https://upload.wikimedia.org/wikipedia/commons/thumb/c/c8/Current_Bank_Logo.png/1280px-Current_Bank_Logo.png" alt="" /></a>
+        <div className={style.left}>
+          <div className={style.logo}>
+            <a href="#"><img src="https://upload.wikimedia.org/wikipedia/commons/thumb/c/c8/Current_Bank_Logo.png/1280px-Current_Bank_Logo.png" alt="" /></a>
+          </div>
+          <div className={style.main}><a href="#"><button className={style.btn}>{langBlock.main}</button></a></div>
+          <div className={style.support}><a href=""><button className={style.btn}>{langBlock.support}</button></a></div>
+          <div className={style.officies}><a href=""><button className={style.btn}>{langBlock.officies}</button></a></div>
         </div>
-        <div className={style.main}><a href="#"><button className="btn">{langBlock.main}</button></a></div>
-        <div className={style.support}><a href=""><button className="btn">{langBlock.support}</button></a></div>
-        <div className={style.officies}><a href=""><button className="btn">{langBlock.officies}</button></a></div>
-        <div className={style.profile}><a href="#"><img src="" alt="" /></a></div>
+        <div className={style.right}>
+          <div className={style.profile}><a href="#"><img src="https://uxwing.com/wp-content/themes/uxwing/download/peoples-avatars/person-icon.png" alt="" /></a></div>
+        </div>
       </div>
     </header>
   )
