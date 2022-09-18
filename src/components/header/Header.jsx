e756ff7dc4f6c14604aa7f08f5e42a9033b7e4d9
GenaEphirov <107998074+GenaEphirov@users.noreply.github.com>
2022-09-18 12:11:57 +0300

    Initial commit

---
+++ src/components/header/Header.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+
+const Header = props => {
+  return (
+    <header>
+      <div className="logo">
+        <a href="#"><img src="https://upload.wikimedia.org/wikipedia/commons/thumb/c/c8/Current_Bank_Logo.png/1280px-Current_Bank_Logo.png" alt="" /></a>
+      </div>
+      <div className="main"><a href="#"></a></div>
+      <div className="support"></div>
+      <div className="profile"></div>
+    </header>
+  )
+}
+
+export default Header;
