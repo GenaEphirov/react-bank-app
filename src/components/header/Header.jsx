f783a02a8691d61cc872e7ce4fc0b4a21168c167
GenaEphirov <107998074+GenaEphirov@users.noreply.github.com>
2022-09-18 14:41:54 +0300

    working on Header(3)

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -9,19 +9,18 @@ const Header = props => {
   // console.log(values)
   return (
     <header>
-      <div className={style.mobilephone1}><img src="" alt=""/><span></span></div>
+      
       <div className={style.container}>
         <div className={style.left}>
           <div className={style.logo}>
             <a href="#"><img src="https://upload.wikimedia.org/wikipedia/commons/thumb/c/c8/Current_Bank_Logo.png/1280px-Current_Bank_Logo.png" alt="" /></a>
           </div>
           <div className={style.main}><a href="#"><button className={style.btn}>{langBlock.main}</button></a></div>
-          <div className={style.support}><a href=""><button className={style.btn}>{langBlock.support}</button></a></div>
-          <div className={style.officies}><a href=""><button className={style.btn}>{langBlock.officies}</button></a></div>
+          <div className={style.support}><a href="#"><button className={style.btn}>{langBlock.support}</button></a></div>
+          <div className={style.officies}><a href="#"><button className={style.btn}>{langBlock.officies}</button></a></div>
         </div>
-        <div className={style.right}>
-          <div className={style.profile}><a href="#"><img src="https://uxwing.com/wp-content/themes/uxwing/download/peoples-avatars/person-icon.png" alt="" /></a></div>
-        </div>
+
+        <div className={style.profile}><a href="#"><img src="https://uxwing.com/wp-content/themes/uxwing/download/peoples-avatars/person-icon.png" alt="" /></a></div>
       </div>
     </header>
   )
